31b1c87565ec00974655add7f5c8c2dec9a81ebd
Steven Bigham <Steven@Bigham.com>
2017-09-18 08:12:23 +0100

    Added Bookmarking Functionaility
    Alt-S

--- background.js
+++ background.js
@@ -1,1 +1,30 @@
 // I'm thinking I might need a background process for context menu reasons mostly
+chrome.commands.onCommand.addListener(command => {
+  if (command === "ScragMark") {
+    addBookmarkToStorage();
+  }
+});
+
+var url = "";
+var title = "";
+var mdText = "";
+
+// Bit pyramidy of doom. Look into promisefying?
+function addBookmarkToStorage() {
+  chrome.storage.local.get('mdText', function (result) {
+    mdText = (result.mdText) ? result.mdText : "";
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      if (tabs.length === 0) return;
+      url = tabs[0].url;
+      title = tabs[0].title;
+
+      mdText += `\n\n[${title}](${url})`;
+      console.log(mdText)
+
+      chrome.storage.local.set({'mdText': mdText}, function() {
+        status.innerHTML = "saved";
+      });
+    });
+  });
+}
--- popup.js
+++ popup.js
@@ -1,12 +1,18 @@
 let mdText = document.getElementById("mdText");
 let status = document.getElementById("status");
 
-chrome.storage.local.get('mdText', function (result) {
-  mdText.value = (result.mdText) ? result.mdText : "";
-});
+function readStored() {
+  chrome.storage.local.get('mdText', function (result) {
+    mdText.value = (result.mdText) ? result.mdText : "";
+  });
+}
+
+readStored();
 
 mdText.addEventListener("keydown", () => status.innerHTML = "Modified");
 document.body.addEventListener("change", saveAll);
+
+chrome.storage.onChanged.addListener(readStored);
 
 function saveAll() {
   chrome.storage.local.set({'mdText': mdText.value}, function() {
